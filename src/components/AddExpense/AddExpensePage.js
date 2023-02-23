fe7a2279ced2d61f53a3edf12b4899c6db4533f0
Macarena Santos <macarena.santos@awsys.com.ar>
2023-02-23 10:52:05 -0300

    remove commented code

--- src/components/AddExpense/AddExpensePage.js
+++ src/components/AddExpense/AddExpensePage.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux'
 import {addExpense} from '../actions/expenses'
 import { connect } from 'react-redux';
 import ExpenseForm from '../ExpenseForm';
@@ -20,12 +19,6 @@ const AddExpensePage = (props) => {
                     }
                 }
                 />
-            {/* <button onClick ={()=>dispatch(addExpense({description:'water bill', amount:100, createdAt: 1250 }))}> Add new water bill </button>
-            <button onClick ={()=>dispatch(addExpense({description:'gas bill', amount:200, createdAt: 700 }))}> Add new gas bill </button>
-            <button onClick ={()=>dispatch(setTextFilter('water'))}> filter bill </button>
-            <button onClick ={()=>dispatch(setStartDate(1250))}> filter start date </button>
-            <button onClick ={()=>dispatch(sortByAmount())}> sort by amount </button>
-             */}
            
         </div>
     );
